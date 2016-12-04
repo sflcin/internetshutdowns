c573583a1dd5c3a1280d5970673d896cdec7464d
arunasank <aruna@mapbox.com>
2016-12-04 12:36:20 +0400

    script with lots of ifs

--- scripts/locate_districts.js
+++ scripts/locate_districts.js
@@ -4,20 +4,25 @@ var districts = require(DATADIR + '/india_district.json');
 var shutdowns = require(DATADIR + '/shutdowns.json');
 
 shutdowns.features = shutdowns.features.map(function (shutdown) {
+    // console.log(shutdown.properties.locality);
     if (shutdown.properties.locality.length) {
+        // console.log(shutdown.properties.locality);
         districts.features.forEach(function (district) {
-            if (shutdown.properties.locality.indexOf(district.properties.NAME_2) > -1 && shutdown.properties.state.indexOf(district.properties.NAME_1) > -1) {
+            if ((shutdown.properties.locality.indexOf(district.properties.NAME_2) > -1 || district.properties.NAME_2.indexOf(shutdown.properties.locality) > -1) || (district.properties.VARNAME_2 && (shutdown.properties.locality.indexOf(district.properties.VARNAME_2) > -1 || district.properties.VARNAME_2.indexOf(shutdown.properties.locality) > -1)) && (shutdown.properties.state.indexOf(district.properties.NAME_1) > -1 || district.properties.NAME_1.indexOf(shutdown.properties.state) > -1)) {
+                // console.log('big if condn');
                 shutdown.geometry = district.geometry;
+                return;
             }
         });
-    } else {
+    } else if (!shutdown.properties.locality.length || !Object.keys(shutdown.properties).length) {
         districts.features.forEach(function (district) {
-            if (shutdown.properties.state.indexOf(district.properties.NAME_1) > -1) {
+            if ((shutdown.properties.state.indexOf(district.properties.NAME_1) > -1) || (district.properties.NAME_1.indexOf(shutdown.properties.state) > -1)) {
                 shutdown.geometry = district.geometry;
+                return;
             }
         });
     }
     return shutdown;
 });
 
-console.log(JSON.stringify(shutdowns));
+console.log(JSON.stringify(shutdowns, null, 2));
