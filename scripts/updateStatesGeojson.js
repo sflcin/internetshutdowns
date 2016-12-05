f79173412ff3e9267ea38fd49d96bc5ea8a825ba
arunasank <aruna@mapbox.com>
2016-12-05 13:14:15 +0530

    add shutdowns info to states

---
+++ scripts/updateStatesGeojson.js
@@ -0,0 +1,16 @@
+'use strict';
+var DATADIR = __dirname + '/../public/data';
+var states = require(DATADIR + '/india_states.json');
+var shutdowns = require(DATADIR + '/shutdowns.json');
+
+states.features = states.features.map(function (state) {
+    state.properties.shutdowns = 0;
+    shutdowns.features.forEach(function (shutdown) {
+        if (state.properties.name.indexOf(shutdown.properties.state) > -1) {
+            state.properties.shutdowns += 1;
+        }
+    });
+    return state;
+});
+
+console.log(JSON.stringify(states, null, 2));
