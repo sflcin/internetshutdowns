f3f2ce4a566a3e522f7267ec1b25340150da2d67
arunasank <aruna@mapbox.com>
2016-12-05 14:47:29 +0530

    find district centroid for shutdown location

--- scripts/locate_districts.js
+++ scripts/locate_districts.js
@@ -2,19 +2,19 @@
 var DATADIR = __dirname + '/../public/data';
 var districts = require(DATADIR + '/india_district.json');
 var shutdowns = require(DATADIR + '/shutdowns.json');
-
+var turfCentroid = require('turf-centroid');
 shutdowns.features = shutdowns.features.map(function (shutdown) {
     if (shutdown.properties.locality.length) {
         districts.features.forEach(function (district) {
             if ((shutdown.properties.locality.indexOf(district.properties.NAME_2) > -1 || district.properties.NAME_2.indexOf(shutdown.properties.locality) > -1) || (district.properties.VARNAME_2 && (shutdown.properties.locality.indexOf(district.properties.VARNAME_2) > -1 || district.properties.VARNAME_2.indexOf(shutdown.properties.locality) > -1)) && (shutdown.properties.state.indexOf(district.properties.NAME_1) > -1 || district.properties.NAME_1.indexOf(shutdown.properties.state) > -1)) {
-                shutdown.geometry = district.geometry;
+                shutdown.geometry = turfCentroid(district.geometry);
                 return;
             }
         });
     } else if (!shutdown.properties.locality.length || !Object.keys(shutdown.properties).length) {
         districts.features.forEach(function (district) {
             if ((shutdown.properties.state.indexOf(district.properties.NAME_1) > -1) || (district.properties.NAME_1.indexOf(shutdown.properties.state) > -1)) {
-                shutdown.geometry = district.geometry;
+                shutdown.geometry = turfCentroid(district.geometry);
                 return;
             }
         });
