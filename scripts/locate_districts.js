1bd871ef2713bd0edbaa022655e7294f699176b6
arunasank <aruna@mapbox.com>
2016-12-05 11:04:57 +0530

    :skull_and_crossbones: logs

--- scripts/locate_districts.js
+++ scripts/locate_districts.js
@@ -4,12 +4,9 @@ var districts = require(DATADIR + '/india_district.json');
 var shutdowns = require(DATADIR + '/shutdowns.json');
 
 shutdowns.features = shutdowns.features.map(function (shutdown) {
-    // console.log(shutdown.properties.locality);
     if (shutdown.properties.locality.length) {
-        // console.log(shutdown.properties.locality);
         districts.features.forEach(function (district) {
             if ((shutdown.properties.locality.indexOf(district.properties.NAME_2) > -1 || district.properties.NAME_2.indexOf(shutdown.properties.locality) > -1) || (district.properties.VARNAME_2 && (shutdown.properties.locality.indexOf(district.properties.VARNAME_2) > -1 || district.properties.VARNAME_2.indexOf(shutdown.properties.locality) > -1)) && (shutdown.properties.state.indexOf(district.properties.NAME_1) > -1 || district.properties.NAME_1.indexOf(shutdown.properties.state) > -1)) {
-                // console.log('big if condn');
                 shutdown.geometry = district.geometry;
                 return;
             }
