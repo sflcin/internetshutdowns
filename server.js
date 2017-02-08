16d5c9460331fa4f73a4de87795cf9b400fbc417
Sarath M S <debian@sarathms.me>
2017-02-08 11:25:09 +0530

    Add routes to handle letsencrypt ACME requests

--- server.js
+++ server.js
@@ -87,6 +87,31 @@ app.post('/shutdown', function (req, res) {
   });
 });
 
+// For LetsEncrypt authorization
+// https://github.com/dmathieu/sabayon
+app.get('/.well-known/acme-challenge/:acmeToken', function(req, res, next) {
+  var acmeToken = req.params.acmeToken;
+  var acmeKey;
+
+  if (process.env.ACME_KEY && process.env.ACME_TOKEN) {
+    if (acmeToken === process.env.ACME_TOKEN) {
+      acmeKey = process.env.ACME_KEY;
+    }
+  }
+
+  for (var key in process.env) {
+    if (key.startsWith('ACME_TOKEN_')) {
+      var num = key.split('ACME_TOKEN_')[1];
+      if (acmeToken === process.env['ACME_TOKEN_' + num]) {
+        acmeKey = process.env['ACME_KEY_' + num];
+      }
+    }
+  }
+
+  if (acmeKey) res.send(acmeKey);
+  else res.status(404).send();
+});
+
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
